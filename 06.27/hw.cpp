410e3ee48d09c5893176fcfb2c33f1c497d1d2e9
V-Sopanha <vsopanha23@gmail.com>
2025-06-29 14:06:49 +0700

    hw.2

--- 06.27/hw.cpp
+++ 06.27/hw.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    
-    for (int r = 0; r < 3; r++) { 
-        for (int i = 1; i <= 12; i++) {
-            for (int c = 1; c <= 4; c++) { 
-                int num = r * 4 + c;
-                cout << num << " x " << i << " = " << num * i << "\t";
+
+    int n = 0;
+    for (int r = 1; r <= 3; r++) { 
+        for (int i = 1; i <= 12 ; i++) {
+            for (int j = 1; j <= 4; j++) {
+                int x = n * 4 + j;
+                cout << x << " x " << i << " = " << i * x << "\t" ;
             }
             cout << "\n";
         }
         cout << "\n";
+        cout << "===========================================================\n\n";
+        ++n;
     }
 
     return 0;
