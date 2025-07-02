8d33ae216fb77ee0cb2cdf2028158a2731af3822
V-Sopanha <vsopanha23@gmail.com>
2025-07-02 22:25:07 +0700

    test

---
+++ 07.02 copy/07.02 copy/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+    for (int i = 1; i <=4; i++){
+        for (int o = 1; o <= i ; o++){
+            cout <<"  ";
+        }
+        for ( int c = 5 - i ; c >=1; c--){
+            cout << c << " ";
+        }
+
+        cout<<"\n";
+    }
