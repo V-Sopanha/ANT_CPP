03855d1ee1378e1e6f16a7d5dd011e5130474563
V-Sopanha <vsopanha23@gmail.com>
2025-07-30 15:39:04 +0700

    dach plerng

---
+++ 07.30/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+class MyClass{
+    string name;
+    int id;
+
+    void Input(){
+        cout<<"Enter Name  : "; getline(cin,name);
+        cout<<"Enter ID    : "; cin>>id; cin.ignore();cin.clear();
+    }
+
+    void Output(){
+        
+    }
+{
+
+}};
+
+MyClass obj[3], obj2;
+
+int main(){
+
+
+}
